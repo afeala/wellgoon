06a0a1c0de1a5f1635d665e6d5158a0b82d11fa9
杨景辉 <jh.yang@csmall.com>
2015-07-25 14:26:57 +0800

    init

--- src/com/wellgoon/action/admin/UserAction.java
+++ src/com/wellgoon/action/admin/UserAction.java
@@ -1,7 +1,12 @@
 package com.wellgoon.action.admin;
 
 import com.opensymphony.xwork2.ActionSupport;
+import com.wellgoon.bean.User;
+import com.wellgoon.service.UserService;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
 
+import javax.annotation.Resource;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 
@@ -12,6 +17,26 @@ import java.awt.event.ActionEvent;
  * 创 建 人：Rocky
  * 创 建 于：2015/7/23
  */
+@Controller("userActionAdmin")
+@Scope("prototype")
 public class UserAction extends ActionSupport {
+    private User user;
+    @Resource
+    private UserService userService;
 
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public UserService getUserService() {
+        return userService;
+    }
+
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
 }
